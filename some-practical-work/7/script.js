89c3fcc616c64169712da616bab6bc470ac46a6a
PrzemekCraker <przemekzbigniew@gmail.com>
2021-02-13 11:47:15 +0100

    array.at(index)-method-ex

---
+++ some-practical-work/7/script.js
@@ -0,0 +1,16 @@
+const fruits = ["orange", "apple", "banana", "grape"];
+
+const lastItem = fruits.at(-1);
+lastItem;
+
+const vegetables = ["potatoe", "tomatoe", "onion"];
+
+vegetables.at(0);
+vegetables.at(1);
+vegetables.at(2);
+vegetables.at(3);
+
+vegetables.at(-1);
+vegetables.at(-2);
+vegetables.at(-3);
+vegetables.at(-4);
